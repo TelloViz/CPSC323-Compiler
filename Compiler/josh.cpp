66858b92bd6390635c4b0a8d50f97e2ec9b36140
Tello- <tellovisionstudio@gmail.com>
2021-09-26 14:04:52 -0700

    Instantiated a Lexer in main entry file

--- Compiler/josh.cpp
+++ Compiler/josh.cpp
@@ -1,11 +1,13 @@
 #include <string>
+#include "LexicalAnalyzer.h"
 
 
 const std::string SOURCE{
 	"while (fahr <= upper) a = 23.00; /* this is sample */"
-}
+};
 
 
+LexicalAnalyzer lex;
 int main()
 {
 	return 0;
