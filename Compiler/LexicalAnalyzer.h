33660398328ab2ae7afa7d64c5e2abc93a6d1436
Tello- <tellovisionstudio@gmail.com>
2021-09-26 14:14:20 -0700

    created eToken, TOKEN_TO_STRING_MAP, and LexicalUnit struct

--- Compiler/LexicalAnalyzer.h
+++ Compiler/LexicalAnalyzer.h
@@ -1,13 +1,35 @@
 #pragma once
 #include <string>
+#include <unordered_map>
 
 namespace LA // Lexical Analysis 
 {
+
+	enum eToken{ACCEPT=0, IDENTIFIER, INTEGER, REAL, KEYWORD, SEPARATOR, BLANK, UNKNOWN};
+
+	const std::unordered_map<eToken, std::string> TOKEN_TO_STRING_MAP{
+		{eToken::ACCEPT, "accept"},
+		{eToken::IDENTIFIER, "identifier"},
+		{eToken::INTEGER, "integer"},
+		{eToken::REAL, "real"},
+		{eToken::KEYWORD, "keyword"},
+		{eToken::SEPARATOR, "separator"},
+		{eToken::BLANK, "blank"},
+		{eToken::UNKNOWN, "unknown"}
+	};
+
+	struct LexicalUnit
+	{
+		eToken token;
+		std::string tokenString;
+		std::string lexeme;
+	};
 
 	class LexicalAnalyzer
 	{
 	public:
 		explicit LexicalAnalyzer(const std::string& sourceRef);
+
 
 
 	private:
