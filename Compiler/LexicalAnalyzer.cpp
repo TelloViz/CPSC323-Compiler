ceef96dd19691fb6d5c6d40cee631b09664353ce
Tello- <tellovisionstudio@gmail.com>
2021-09-26 14:07:39 -0700

    Wrapped lexer in namespace and created a ctor

---
+++ Compiler/LexicalAnalyzer.cpp
@@ -0,0 +1,5 @@
+#include "LexicalAnalyzer.h"
+
+LexicalAnalyzer::LexicalAnalyzer(const std::string& sourceRef)
+{
+}
--- Compiler/LexicalAnalyzer.h
+++ Compiler/LexicalAnalyzer.h
@@ -1,5 +1,17 @@
 #pragma once
-class LexicalAnalyzer
-{
-};
+#include <string>
 
+namespace LA // Lexical Analysis 
+{
+
+	class LexicalAnalyzer
+	{
+	public:
+		explicit LexicalAnalyzer(const std::string& sourceRef);
+
+
+	private:
+
+	};
+
+}
