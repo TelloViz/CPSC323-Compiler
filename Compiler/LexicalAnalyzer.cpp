8c67c9c8bad0bd935af226fd4fe2f391e01a168d
Tello- <tellovisionstudio@gmail.com>
2021-09-27 23:23:17 -0700

    Added a whole lot of code today.
    Made state table
    lots of classes for different data types

--- Compiler/LexicalAnalyzer.cpp
+++ Compiler/LexicalAnalyzer.cpp
@@ -1,10 +1,14 @@
 #include "LexicalAnalyzer.h"
 
 LA::LexicalAnalyzer::LexicalAnalyzer(const std::string& sourceRef)
+	: m_source{ sourceRef }, m_currentIndex{ 0 }
 {
 }
 
 LA::LexicalUnit LA::LexicalAnalyzer::Lexer()
 {
-	return LexicalUnit();
+	if( !IsEOF && !IsBlank())
+	{
+		
+	}
 }
--- Compiler/LexicalAnalyzer.h
+++ Compiler/LexicalAnalyzer.h
@@ -1,17 +1,35 @@
 #pragma once
 #include <string>
 #include <unordered_map>
+#include <list>
+#include <algorithm>
 
 namespace LA // Lexical Analysis 
 {
 
-	enum eToken
-	// Valid Rat21F tokens
+	enum eStates
 	{
-		ACCEPT=0, 
+		START = 0,
+		IDENTIFIER = 1,
+		INTEGER = 2,
+		INCOMPLETE_REAL = 3,
+		REAL = 4,
+		UNKNOWN = 5
+	};
+
+	std::list<int> acceptanceStates
+	{ 
+		eStates::IDENTIFIER, 
+		eStates::INTEGER, 
+		eStates::REAL, 
+		eStates::UNKNOWN };
+
+	enum eToken
+		// Valid Rat21F tokens
+	{
 		IDENTIFIER, 
-		INTEGER, 
-		REAL, 
+		INTEGER,
+		REAL,
 		KEYWORD, 
 		SEPARATOR, 
 		BLANK, 
@@ -21,7 +39,6 @@ namespace LA // Lexical Analysis
 	const std::unordered_map<eToken, std::string> TOKEN_TO_STRING_MAP
 	// Map used to convert eToken enumerated values into string values for output
 	{
-		{eToken::ACCEPT, "accept"},
 		{eToken::IDENTIFIER, "identifier"},
 		{eToken::INTEGER, "integer"},
 		{eToken::REAL, "real"},
@@ -41,8 +58,8 @@ namespace LA // Lexical Analysis
 
 	
 
-	const int NUM_STATES{ 5 };
-	const int NUM_INPUTS{ 4 };
+	const int NUM_STATES{ 6 };
+	const int NUM_INPUTS{ 3 };
 
 	class LexicalAnalyzer
 	// Main class of the lexer phase. This is the LexicalAnalyzer itself.
@@ -53,13 +70,32 @@ namespace LA // Lexical Analysis
 		LexicalUnit Lexer();
 
 	private:
+
+		bool IsEOF() const { return m_source[m_currentIndex] == NULL; }
+		bool IsBlank() const { return isspace(m_source[m_currentIndex]); }
+		bool IsAccepted() const { return std::find(acceptanceStates.begin(), acceptanceStates.end(), m_currentState) != acceptanceStates.end(); }
+
+	private:
 		
+		int m_currentIndex;
+		eStates m_currentState{ eStates::START };
+		const std::string m_source;
+
+		std::list<std::string> m_delimiters{ ";", " ", "\t", "\n" };
+		
+
 		//, L, D, \.
-		int StateTable[NUM_STATES][NUM_INPUTS] = 
+		int StateTable[NUM_STATES][NUM_INPUTS+1] = 
 		{
-			/*             Input                             */
-			/*             L  D  .                           */
-			/* State */ 
+			/*                                                         Input                                           */
+
+			/*               Start                              Letter		               Digit               Period(.)                           */
+			/* State 0 */	  eStates::START,			eStates::IDENTIFIER,	   eStates::INTEGER,	 eStates::UNKNOWN,
+			/* State 1 */	  eStates::IDENTIFIER,		eStates::IDENTIFIER,	   eStates::IDENTIFIER,   eStates::UNKNOWN,
+			/* State 2 */	  eStates::INTEGER,			eStates::UNKNOWN,		   eStates::INTEGER,	 eStates::INCOMPLETE_REAL,
+			/* State 3 */	  eStates::INCOMPLETE_REAL,   eStates::UNKNOWN,		   eStates::REAL,		 eStates::UNKNOWN,
+			/* State 4 */	  eStates::REAL,			eStates::UNKNOWN,	        eStates::REAL,		 eStates::UNKNOWN,
+			/* State 5 */	  eStates::UNKNOWN,			eStates::UNKNOWN,		   eStates::UNKNOWN,	 eStates::UNKNOWN
 		};
 
 	};
