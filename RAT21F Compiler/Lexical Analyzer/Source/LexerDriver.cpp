2daa693b92213fbeee493b804a6226ccebd1476d
Tello- <tellovisionstudio@gmail.com>
2021-10-08 16:54:03 -0700

    Source Stripper works great and is now integrated into dev branch successfully

--- RAT21F Compiler/Lexical Analyzer/Source/LexerDriver.cpp
+++ RAT21F Compiler/Lexical Analyzer/Source/LexerDriver.cpp
@@ -1,7 +1,14 @@
 #include <iostream>
+#include "../Include/Utility/SourceStripper.hpp"
 
-
+std::string SOURCE{ "This is my \n Source \n\nCode. Thre are a few lines\n in it" };
 int main()
 {
+	auto newSource = Utility::SplitStringByLine(SOURCE);
+
+	for (auto iter : newSource)
+	{
+		std::cout << iter << std::endl;
+	}
 	return 0;
 }
