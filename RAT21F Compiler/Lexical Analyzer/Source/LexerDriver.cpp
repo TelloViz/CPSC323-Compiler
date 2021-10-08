b601380b9fa781bedde7f1d75922482e751b7d5d
Tello- <tellovisionstudio@gmail.com>
2021-10-08 15:57:35 -0700

    Moved stripper code from sourcestripper to /dev/

--- RAT21F Compiler/Lexical Analyzer/Source/LexerDriver.cpp
+++ RAT21F Compiler/Lexical Analyzer/Source/LexerDriver.cpp
@@ -1,6 +1,21 @@
 #include <iostream>
 
 
+std::vector<std::string> Utility::SplitStringByLine(const std::string& str)
+{
+	std::stringstream inSS(str);
+	std::vector<std::string> returnVec{};
+
+	std::string line;
+	while (std::getline(inSS, line))
+	{
+		if (line.size()) returnVec.push_back(line);
+	}
+	return returnVec;
+}
+
+
+
 int main()
 {
 	return 0;
