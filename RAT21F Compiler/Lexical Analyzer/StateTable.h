ff8ac604331322cc46e2db65f6c242039bbe9eaa
Tello- <tellovisionstudio@gmail.com>
2021-10-10 15:56:20 -0700

    Initalized the state table completely now I think. Build successfull

--- RAT21F Compiler/Lexical Analyzer/StateTable.h
+++ RAT21F Compiler/Lexical Analyzer/StateTable.h
@@ -2,21 +2,22 @@
 //#include "Include/State.h"
 #include <vector>
 
-class Transition
+struct Transition
 {
 public:
-	Transition(StateTable::eInputType inputTrigger, int targetState) : m_inputTrigger{ inputTrigger }, m_targetStateID{ targetState } {}
+	Transition(StateTable::eInputType inputTrigger, int targetState) : m_inputTrigger( inputTrigger ), m_targetStateID( targetState ) {}
+public:
 	StateTable::eInputType m_inputTrigger;
 	int m_targetStateID;
 };
 
-class State
+struct State
 {
 public:
 	State(int ID, bool isAcceptState, std::vector<Transition> transVec);
 	int ID() const { return m_stateID; }
 	bool IsAcceptState() const { return m_isAcceptState; }
-private:
+
 	int m_stateID;
 	bool m_isAcceptState;
 	std::vector<Transition> m_transVec;
@@ -32,50 +33,7 @@ public:
 
 private:
 
-	std::vector<State> m_states
-	{
-		State( 0, false,		
-			std::vector<Transition>
-				{
-					Transition(eInputType::LETTER, 1),
-					Transition(eInputType::DIGIT, 2)
-				}
-		),
-		State( 1, true,
-			std::vector<Transition>
-				{
-					Transition(eInputType::LETTER, 1),
-					Transition(eInputType::DIGIT, 1),
-					Transition(eInputType::UNDERSCORE, 1)
-				}
-		),
-		State(2, true,
-		std::vector<Transition>
-			{
-				Transition(eInputType::DIGIT, 2),
-				Transition(eInputType::PERIOD, 3)
-			}
-		),
-		State(3, false,
-		std::vector<Transition>
-			{
-				Transition(eInputType::DIGIT, 4)
-			}
-		),
-		State(4, true,
-		std::vector<Transition>
-			{
-				Transition(eInputType::DIGIT, 4)
-			}
-		)
-	};
-	
-	std::vector<State> m_stateVec
-	{
-		State()
-
-	}
-	
+	std::vector<State> m_states;
 
 
 };
